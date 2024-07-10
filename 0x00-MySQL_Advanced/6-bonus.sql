c703d4a20fb748186b3e1e6de9c834d164441c5d
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-10 20:07:52 +0000

    test

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,14 @@
+-- 6-bonus.sql
+DELIMITER //
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE project_id INT;
+    IF NOT EXISTS (SELECT * FROM projects WHERE name = project_name) THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    ELSE
+        SELECT id INTO project_id FROM projects WHERE name = project_name;
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END //
+DELIMITER ;
