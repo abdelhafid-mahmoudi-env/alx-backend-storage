24b0a454a34033cc80416ca50f59d5e287315f55
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-17 18:16:19 +0000

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,52 +1,38 @@
 #!/usr/bin/env python3
-"""
-A module for request caching and tracking.
-"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
 
-redis_client = redis.Redis()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def cache_response(method: Callable) -> Callable:
-    """
-    Decorator to cache the response
-    """
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def wrapper(url: str) -> str:
-        """
-        Wrapper function to handle caching and request tracking.
-        """
-        if not isinstance(url, str):
-            raise TypeError("URL must be a string")
-        
-        redis_client.incr(f"count:{url}")
-        
-        cached_result = redis_client.get(f"result:{url}")
-        if cached_result:
-            return cached_result.decode("utf-8")
-        
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
         result = method(url)
-        
-        if not isinstance(result, str):
-            raise ValueError("The result must be a string")
-        
-        redis_client.setex(f"result:{url}", 10, result)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
-    return wrapper
+    return invoker
 
 
-@cache_response
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    Fetches the content of a URL
-    """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.text
-    except requests.RequestException as e:
-        raise RuntimeError(f"Error fetching the URL {url}: {e}")
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
