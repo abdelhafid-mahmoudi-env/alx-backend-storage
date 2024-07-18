82369c5c379b4ef897284d7507ef3f87963ed17e
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-18 01:55:15 +0000

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
-"""Module for implementing an expiring."""
+"""Module for implementing"""
 import redis
 import requests
-from typing import Optional
+from typing import Callable
+from functools import wraps
 
 
 r = redis.Redis()
 
 
+def cache_with_tracking(expiration: int):
+    """Decorator to cache the result of a function."""
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(url: str) -> str:
+            count_key = f"count:{url}"
+            r.incr(count_key)
+            cached_html = r.get(url)
+            if cached_html:
+                return cached_html.decode('utf-8')
+            result = func(url)
+            r.setex(url, expiration, result)
+            return result
+        return wrapper
+    return decorator
+
+
+@cache_with_tracking(expiration=10)
 def get_page(url: str) -> str:
-    """Obtain the HTML content of a particular URL."""
-    count_key = f"count:{url}"
-    r.incr(count_key)
-    cached_html: Optional[bytes] = r.get(url)
-    if cached_html:
-        return cached_html.decode('utf-8')
+    """Obtain the HTML content of a particular."""
     response = requests.get(url)
-    html_content = response.text
-    r.setex(url, 10, html_content)
-    return html_content
+    return response.text
 
 
 if __name__ == "__main__":
-    test_url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(test_url))
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.com"
+    print(get_page(url))
