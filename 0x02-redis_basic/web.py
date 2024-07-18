fd939f749fc20bc5a5d577f4468c9c2abdd488cc
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-18 01:58:17 +0000

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
-'''A module with tools for request.'''
+"""This module contains."""
 import redis
 import requests
+from typing import Optional, Callable
 from functools import wraps
-from typing import Callable
 
 
-redis_store = redis.Redis()
+def count_url_calls(fn: Callable) -> Callable:
+    @wraps(fn)
+    def increment_url_count(*args, **kwargs):
+        r: redis.Redis = redis.Redis()
+        url: str = ''
+        if args:
+            url = args[0]
+        if kwargs:
+            url = kwargs["url"] if "url" in kwargs else url
+        r.incr("count:{}".format(url))
+        return fn(*args, **kwargs)
+    return increment_url_count
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched.'''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function'''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
+@count_url_calls
 def get_page(url: str) -> str:
-    '''Returns the content of a URL.'''
-    return requests.get(url).text
+    r: redis.Redis = redis.Redis()
+    page_cache: Optional[bytes] = r.get(url)
+    html_content: Optional[str] = ''
+    if page_cache:
+        html_content = page_cache.decode('utf-8')
+    else:
+        html_content = requests.get(url).text
+        r.setex(url, 10, html_content)
+    return html_content
