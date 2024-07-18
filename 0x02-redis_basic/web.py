1b51012a879c281bdfbbb9df5974351465abed17
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-18 02:19:59 +0000

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -46,5 +46,3 @@ def get_page(url: str) -> str:
 if __name__ == "__main__":
     url = 'http://slowwly.robertomurray.co.uk'
     print(get_page(url))
-    print(get_page(url))
-    print(f"URL {url} accessed {redis_instance.get(f'count:{url}').decode('utf-8')} times")
