2735c33716b13ef2a04a66febf9fe891b5cbdb57
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-18 01:45:38 +0000

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -34,3 +34,10 @@ def get_page(url: str) -> str:
     """
     response = requests.get(url)
     return response.text
+
+
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk"
+    print(get_page(url))
+    print(get_page(url))
+    print(get_page(url))
