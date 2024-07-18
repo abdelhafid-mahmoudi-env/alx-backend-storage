7992f3fcc5b754c569b2d0d521ed8b33914ee508
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-18 02:33:30 +0000

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,34 @@
 #!/usr/bin/env python3
-"""
-This module provides functionality to fetch HTML content from a URL,
-while counting requests and caching responses using Redis.
-"""
-
+"""This module provides a function to fetch."""
 import requests
 import redis
-from typing import Callable, Any
+from typing import Callable
 import functools
 
-# Initialize Redis instance
+
 redis_instance = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """
-    Decorator to count how many times a URL is requested.
-    
-    Args:
-        method (Callable): The method to be decorated.
-    
-    Returns:
-        Callable: The decorated method.
-    """
+    """Decorator to count how many times"""
     @functools.wraps(method)
-    def wrapper(url: str, *args: Any, **kwargs: Any) -> str:
+    def wrapper(url: str) -> str:
         count_key = f"count:{url}"
         redis_instance.incr(count_key)
-        return method(url, *args, **kwargs)
+        return method(url)
     return wrapper
 
 
 def cache_page(expiration: int) -> Callable:
-    """
-    Decorator to cache the HTML content of a URL.
-    
-    Args:
-        expiration (int): The cache expiration time in seconds.
-    
-    Returns:
-        Callable: The decorated method.
-    """
+    """Decorator to cache the HTML content"""
     def decorator(method: Callable) -> Callable:
         @functools.wraps(method)
-        def wrapper(url: str, *args: Any, **kwargs: Any) -> str:
+        def wrapper(url: str) -> str:
             cache_key = f"cache:{url}"
             cached_content = redis_instance.get(cache_key)
             if cached_content:
                 return cached_content.decode('utf-8')
-            html_content = method(url, *args, **kwargs)
+            html_content = method(url)
             redis_instance.setex(cache_key, expiration, html_content)
             return html_content
         return wrapper
@@ -56,22 +36,11 @@ def cache_page(expiration: int) -> Callable:
 
 
 @count_requests
-@cache_page(expiration=600)  # Set expiration to 10 minutes
+@cache_page(expiration=10)
 def get_page(url: str) -> str:
-    """
-    Fetch the HTML content of a URL.
-    
-    Args:
-        url (str): The URL to fetch.
-    
-    Returns:
-        str: The HTML content of the URL.
-    
-    Raises:
-        HTTPError: An error occurred accessing the URL.
-    """
+    """Fetch the HTML content of a URL"""
     response = requests.get(url)
-    response.raise_for_status()  # Ensure we handle potential HTTP errors
+    response.raise_for_status()
     return response.text
 
 
